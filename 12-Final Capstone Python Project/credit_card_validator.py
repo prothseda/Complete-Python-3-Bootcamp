3d95a257359b29a48675fc5067a5b2e576ea66c0
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-02 15:57:53 +1100

    created a working Luhn Algorithm Validation tool.

--- 12-Final Capstone Python Project/credit_card_validator.py
+++ 12-Final Capstone Python Project/credit_card_validator.py
@@ -21,28 +21,67 @@ Possible additions:
 '''
 
 
+class LuhnCheck:
 
+    def __init__(self,credit_card_number):
+        # Transform the input credit_card_number string into a list of integers
+        self.credit_card_number = []
+        for number in credit_card_number:
+            self.credit_card_number.append(int(number))
+        
+        # Create a dictionary for the doubling of specific key values
+        self.double_dictionary = {}
 
+    def __str__(self):
+        # Used for debugging
+        return f'{self.credit_card_number}'
+
+    def double_every_second_digit(self):
+        # Starting from the rightmost digit, double the value of every second digit
+        index_count = 0
+        # double_dictionary = {}
+
+        for number in reversed(self.credit_card_number): # reversed so we begin at the rightmost digit
+            if index_count % 2 == 0: # python is zero based which is considered even!
+                self.double_dictionary[index_count] = number
+            else: # these must be odd (our second digits)
+                self.double_dictionary[index_count] = number * 2
+
+                # If doubling of a number results in a value greater than 9 then add the digits of the product
+                if self.double_dictionary[index_count] > 9:
+                    a,b = str(self.double_dictionary[index_count])
+                    self.double_dictionary[index_count] = int(a) + int(b)
+
+            index_count += 1
+
+        return f'{self.double_dictionary}' # debug
+
+    def sum_of_all_digits(self):
+        # Now take the sum of all the digits (including non-doubled)
+        return sum(self.double_dictionary.values())
+
+    def validate(self):
+        # Returns True if the credit_card_number passes the LuhnCheck
+        return self.sum_of_all_digits() % 2 == 0
 
 # User inputs a Credit Card Number
-
-
-# Validate that the input string is all numeric
-
-
-# Step 1 – Starting from the rightmost digit, double the value of every second digit
-
-
-# Step 2 – If doubling of a number results in a value greater than 9 then add the digits of the product
-
-
-# Step 3 – Now take the sum of all the digits (including non-doubled)
-
-
-# Step 4 – If the total modulo 10 is equal to 0 then the number is valid else it is not valid
-
-
-# Inform the User if the input Credit Card Number is valid or not
+while True:
+    try:
+        input_credit_card = input('Please provide a Credit Card Number: ')
+        int(input_credit_card) # Validate that the input string is all numeric
+    except TypeError:
+        print(f'{input_credit_card} is an invalid input. Please double-check and try again.')
+    except:
+        print(f'An unknown error occured.')
+    else:
+        check = LuhnCheck(input_credit_card)
+        # Inform the User if the input Credit Card Number is valid or not
+        if check.validate():
+            print(f'{input_credit_card} is a possibly valid Credit Card Number.')
+            break
+        else:
+            print(f'{input_credit_card} is not a valid Credit Card Number.')
+            break
 
 
 
