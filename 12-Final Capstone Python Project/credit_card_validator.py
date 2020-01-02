6b220f90d1d650bb7b5cffda255617e496af3c6d
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-02 20:55:04 +1100

    added determination of common card types.

--- 12-Final Capstone Python Project/credit_card_validator.py
+++ 12-Final Capstone Python Project/credit_card_validator.py
@@ -16,8 +16,14 @@ Example:
     modulo 10 of 70 equals 0
     79927398713 is valid.
 
-Possible additions:
-    * Detection of card type (Visa or MasterCard)
+Addition - detection of care type:
+    The first digit in your credit-card number signifies the system: 
+        3 - travel/entertainment cards (such as American Express and Diners Club)
+            37 = American Express
+            38 = Diners Club
+        4 - Visa
+        5 - MasterCard
+        6 - Discover Card
 '''
 
 
@@ -64,6 +70,19 @@ class LuhnCheck:
         # Returns True if the credit_card_number passes the LuhnCheck
         return self.sum_of_all_digits() % 10 == 0
 
+    def card_type(self):
+        # Returns the Card Type, if it can be determined
+        card_types = {3:{7:'American Express', 8:'Diners Club'}, 4:'Visa', 5:'MasterCard', 6:'Discover Card'}
+
+        if self.credit_card_number[0] == 3 and self.credit_card_number[1] in (7, 8):
+            card_type = card_types[self.credit_card_number[0]][self.credit_card_number[1]]
+            return f'This appears to be a {card_type}.'
+        elif self.credit_card_number[0] in (4, 5, 6):
+            card_type = card_types[self.credit_card_number[0]]
+            return f'This appears to be a {card_type}.'
+        else:
+            return f'Unable to determine the card type.'
+        
 # User inputs a Credit Card Number
 while True:
     try:
@@ -78,6 +97,7 @@ while True:
         # Inform the User if the input Credit Card Number is valid or not
         if check.validate():
             print(f'{input_credit_card} is a possibly valid Credit Card Number.')
+            print(check.card_type())
             break
         else:
             print(f'{input_credit_card} is not a valid Credit Card Number.')
