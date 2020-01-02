61e33761e16c95b1c317d3f98bca664bc76f06fe
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-02 14:52:57 +1100

    initial skeleton and planning

---
+++ 12-Final Capstone Python Project/credit_card_validator.py
@@ -0,0 +1,56 @@
+'''
+Numbers Capstone Project: Credit Card Validator
+Takes in a credit card number from a common credit card vendor
+and validates it to make sure that it is a valid number
+
+This is acomplished by using the Luhn Algorithm: https://en.wikipedia.org/wiki/Luhn_algorithm
+    Step 1 – Starting from the rightmost digit, double the value of every second digit
+    Step 2 – If doubling of a number results in a value greater than 9 then add the digits of the product
+    Step 3 – Now take the sum of all the digits (including non-doubled)
+    Step 4 – If the total modulo 10 is equal to 0 then the number is valid else it is not valid
+
+Example:
+    Starting with 79927398713
+    We get 79947697723
+    Sum is 70
+    modulo 10 of 70 equals 0
+    79927398713 is valid.
+
+Possible additions:
+    * Detection of card type (Visa or MasterCard)
+'''
+
+
+
+
+
+# User inputs a Credit Card Number
+
+
+# Validate that the input string is all numeric
+
+
+# Step 1 – Starting from the rightmost digit, double the value of every second digit
+
+
+# Step 2 – If doubling of a number results in a value greater than 9 then add the digits of the product
+
+
+# Step 3 – Now take the sum of all the digits (including non-doubled)
+
+
+# Step 4 – If the total modulo 10 is equal to 0 then the number is valid else it is not valid
+
+
+# Inform the User if the input Credit Card Number is valid or not
+
+
+
+
+ 
+
+
+
+
+
+
